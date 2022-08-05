3e26abfb595767999f3d0ae6c0b563de3ce82f03
Irene Boyko <irina@trellis.co>
2022-08-05 17:57:11 +0300

    SECTION-3: add main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from "./header/header.component";
+import { RecipesComponent } from './recipesPage/recipes/recipes.component';
+import { RecipeListComponent } from './recipesPage/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipesPage/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipesPage/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shoppingListPage/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shoppingListPage/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
