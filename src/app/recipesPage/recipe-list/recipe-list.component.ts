dcd991f5e9fef8d2118bbffded7344683e579691
Irene Boyko <irina@trellis.co>
2022-08-05 19:57:10 +0300

    SECTION-3: add basic content to components

--- src/app/recipesPage/recipe-list/recipe-list.component.ts
+++ src/app/recipesPage/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../../shared/models/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is a test recipe', 'https://gotovim-doma.ru/images/recipe/e/4c/e4cad715fa2c3406c8bce678272beed8_l.jpg'),
+    new Recipe('Test Recipe', 'This is a test recipe', 'https://gotovim-doma.ru/images/recipe/e/4c/e4cad715fa2c3406c8bce678272beed8_l.jpg'),
+    new Recipe('Test Recipe', 'This is a test recipe', 'https://gotovim-doma.ru/images/recipe/e/4c/e4cad715fa2c3406c8bce678272beed8_l.jpg'),
+    new Recipe('Test Recipe', 'This is a test recipe', 'https://gotovim-doma.ru/images/recipe/e/4c/e4cad715fa2c3406c8bce678272beed8_l.jpg'),
+  ];
 
   constructor() { }
 
