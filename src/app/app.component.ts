457a5c47f23c2f4736427fd9c19d32f666575b5c
Irene Boyko <irina@trellis.co>
2022-08-08 20:52:41 +0300

    SECTION-6: add custom routes, updated recipes loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  currentPage: string = 'recipes';
+
+  onNavigate(target: string) {
+    this.currentPage = target;
+  }
 }
